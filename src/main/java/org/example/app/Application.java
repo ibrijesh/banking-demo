1bd96d09fd89244a73755c497fb6f65f58b2ced9
Brijesh <brijesh.yadav@credable.in>
2022-02-22 10:00:41 +0530

    added beans

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,35 +1,31 @@
 package org.example.app;
 
+import org.example.AppConfig;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepositoryDB;
 import org.example.repository.CustomerRepositoryStub;
 import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
-// IoC container
 public class Application {
 
     public static void main(String[] args) {
 
-        // DESIGN PRINCIPLE: Always program to interface and not to an implementation
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // wiring your application
-        CustomerService service = new DefaultCustomerService(newCustomerRepositoryStub());
+        CustomerService service = context.getBean("customerService", CustomerService.class);
 
+//    CustomerService service = new DefaultCustomerService(new CustomerRepositoryDB());
 //    List<Customer> customers = service.getAllCustomersWithStars();
         List<Customer> customers = service.getAllCustomers();
 
         customers.forEach(System.out::println);
     }
 
-    public static CustomerRepositoryDB newCustomerRepositoryDB() {
-        return new CustomerRepositoryDB();
-    }
 
-    public static CustomerRepositoryStub newCustomerRepositoryStub() {
-        return new CustomerRepositoryStub();
-    }
 
 }
