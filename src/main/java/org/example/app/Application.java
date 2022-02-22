d2c1bb27bc87999d69800e1efad4cd9f8e9967d6
Brijesh <brijesh.yadav@credable.in>
2022-02-22 08:21:06 +0530

    added CutomerRepository and CustomrepositoryStub

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,18 +1,35 @@
 package org.example.app;
 
 import org.example.model.Customer;
+import org.example.repository.CustomerRepositoryDB;
+import org.example.repository.CustomerRepositoryStub;
+import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
 
 import java.util.List;
 
+// IoC container
 public class Application {
 
     public static void main(String[] args) {
-        DefaultCustomerService service = new DefaultCustomerService();
 
+        // DESIGN PRINCIPLE: Always program to interface and not to an implementation
+
+        // wiring your application
+        CustomerService service = new DefaultCustomerService(newCustomerRepositoryStub());
+
+//    List<Customer> customers = service.getAllCustomersWithStars();
         List<Customer> customers = service.getAllCustomers();
 
         customers.forEach(System.out::println);
-
     }
+
+    public static CustomerRepositoryDB newCustomerRepositoryDB() {
+        return new CustomerRepositoryDB();
+    }
+
+    public static CustomerRepositoryStub newCustomerRepositoryStub() {
+        return new CustomerRepositoryStub();
+    }
+
 }
--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -1,24 +1,31 @@
 package org.example.service;
 
 import org.example.model.Customer;
+import org.example.repository.CustomerRepository;
 import org.example.repository.CustomerRepositoryDB;
+import org.example.repository.CustomerRepositoryStub;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DefaultCustomerService {
+public class DefaultCustomerService  implements CustomerService {
 
-private CustomerRepositoryDB repository = new CustomerRepositoryDB();
+        private CustomerRepository repository;
 
-public List<Customer> getAllCustomers() {
-return repository.findAll();
+        // injecting the repository using the constructor
+        public DefaultCustomerService(CustomerRepository repository) {
+                this.repository = repository;
+        }
+
+        public List<Customer> getAllCustomers() {
+                return repository.findAll();
+        }
+
+        public List<Customer> getAllCustomersWithStars() {
+                return repository.findAll()
+                        .stream()
+                        .map(c -> new Customer(c.getId(), "#####"+c.getName()+"****", c.getCity()))
+                        .collect(Collectors.toList());
+        }
+
 }
-
-public List<Customer> getAllCustomersWithStars() {
-        return repository.findAll()
-                .stream()
-                .map(c -> new Customer(c.getId(), "****"+c.getName()+"****", c.getCity()))
-                .collect(Collectors.toList());
-}
-
-} 
